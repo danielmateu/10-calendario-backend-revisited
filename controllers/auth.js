d141d882c43ad5873bd30e6bfe6352285210109c
Daniel Mateu <danielmateu86@gmail.com>
2023-03-31 10:48:05 +0200

    Creación de usuario en base de datos

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,28 @@
 const { response } = require('express');
+const Usuario = require('../models/Usuario');
 
-const crearUsuario = (req, res = response) => {
+const crearUsuario = async (req, res = response) => {
 
-    const { name, email, password } = req.body;
+    // const { name, email, password } = req.body;
+    try {
+        // Crear nueva instancia del usuario
+        const usuario = new Usuario(req.body);
 
-    res.status(201).json({
-        ok: true,
-        message: 'crearUsuario',
-        // user: req.body
-        name,
-        email,
-        password
-    });
+        // Guardar en la BD
+        await usuario.save();
+
+        res.status(201).json({
+            ok: true,
+            message: 'crearUsuario',
+        });5
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            message: 'Hable con el administrador'
+        });
+    }
 }
 
 const loginUsuario = (req, res = response) => {
