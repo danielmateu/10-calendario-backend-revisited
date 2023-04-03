5ba0debc9501653c3c3e3f5b953b08db54088697
Daniel Mateu <danielmateu86@gmail.com>
2023-04-03 17:33:57 +0200

    a ver...

--- index.js
+++ index.js
@@ -24,6 +24,10 @@ app.use('/api/auth', require('./routes/auth'));
 // Todo: CRUD: Eventos
 app.use('/api/events', require('./routes/events'));
 
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname + '/public/index.html'));
+});
+
 // Escuchar peticiones
 app.listen(process.env.PORT, () => {
     console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
